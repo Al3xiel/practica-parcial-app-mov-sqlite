819ac6dfb60996bd868dac2a86dafbfb98380634
Al3xiel <gabriel280104casas@gmail.com>
2025-05-18 23:19:05 -0500

    feat(nav): implemented navigator.

--- app/src/main/java/com/example/practica_parcial_swqlite/MainActivity.kt
+++ app/src/main/java/com/example/practica_parcial_swqlite/MainActivity.kt
@@ -4,44 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import com.example.practica_parcial_swqlite.model.viewmodel.HeroViewModel
 import com.example.practica_parcial_swqlite.ui.theme.PracticaparcialswqliteTheme
+import com.example.practica_parcial_swqlite.views.nav.Navigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val viewModel by viewModels<HeroViewModel>()
         setContent {
             PracticaparcialswqliteTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+               Navigation(viewModel, this)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PracticaparcialswqliteTheme {
-        Greeting("Android")
     }
 }
